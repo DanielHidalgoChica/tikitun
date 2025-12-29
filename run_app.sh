5bbe59051e34bc9d44466c83e637e72af9b325cc
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2025-12-29 13:13:12 +0100

    Añadida instalación requerimientos en ./run_app

--- run_app.sh
+++ run_app.sh
@@ -4,5 +4,6 @@ export IC_HOME=/opt/oracle/instantclient_23_26
 export LD_LIBRARY_PATH="$IC_HOME:/usr/lib/x86_64-linux-gnu"
 ldd /opt/oracle/instantclient_23_26/libsqora.so.23.1
 source .venv/bin/activate
+pip install -r requirements.txt > /dev/null
 #exec streamlit run app.py
 exec python app_tkinter_pedidos.py
