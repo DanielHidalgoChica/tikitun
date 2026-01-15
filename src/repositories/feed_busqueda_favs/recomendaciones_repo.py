6be2cb81bd5ed340f3ac9fd3d5e3a381ff405019
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2026-01-15 20:25:20 +0100

    Terminada la parte de favoritos. Ahora mismo en el feed se consultan todos los productos de la base de datos

--- src/repositories/feed_busqueda_favs/recomendaciones_repo.py
+++ src/repositories/feed_busqueda_favs/recomendaciones_repo.py
@@ -23,8 +23,43 @@ def get_recomendaciones(cn, username: str) -> list[dict]:
     
     Returns:
         Lista ordenada de productos
+    
+
+    Por ahora lo único que hace es pillar todos los productos disponibles de la base de datos
+    Returns:
+        Lista de todos los productos con disponible = 1
     """
     print("   [REPO recomendaciones] get_recomendaciones()", username)
+    cur = cn.cursor()
+    cur.execute("""
+        SELECT 
+            p.id_producto,
+            p.titulo,
+            p.descripcion,
+            p.precio,
+            p.username AS username_vendedor,
+            p.nombre_categoria,
+            p.promocion,
+            p.disponible
+        FROM producto p
+        WHERE p.disponible = 1
+        ORDER BY p.id_producto DESC
+    """)
+    
+    productos = []
+    for row in cur.fetchall():
+        productos.append({
+            "id_producto": row[0],
+            "titulo": row[1],
+            "descripcion": row[2],
+            "precio": row[3],
+            "username_vendedor": row[4],
+            "nombre_categoria": row[5],
+            "promocion": row[6],
+            "disponible": row[7]
+        })
+    
+    cur.close()
+    return productos   
     # TODO: Implementar consulta compleja
     # TODO: Usar función de distancia geográfica (ej: Haversine)
-    return []  # Demo
