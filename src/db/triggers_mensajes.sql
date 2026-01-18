93b8f3ac4ca625f9e8f690ffd98c665df5a3d1c6
Megax47 <aitordelaiglesia@gmail.com>
2026-01-18 01:59:41 +0100

    Creado trigger para archivado automático

--- src/db/triggers_mensajes.sql
+++ src/db/triggers_mensajes.sql
@@ -1,3 +1,12 @@
 
 CREATE OR REPLACE TRIGGER TR_Archivado_Al_Finalizar
-AFTER UPDATE ON 
+AFTER UPDATE OF recepcion_confirmada
+ON Vendido
+FOR EACH ROW
+WHEN (NEW.recepcion_confirmada = 1 AND OLD.recepcion_confirmada <> 1)
+BEGIN
+    UPDATE Chat
+    SET archivado = 1
+    WHERE id_producto = :NEW.id_producto;
+END;
+/
