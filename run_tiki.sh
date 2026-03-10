a726b15b324e56de5964c017a21841459d8fb25f
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2026-03-10 15:56:03 +0100

    pdf and zip out, run_tiki cleaned up

--- run_tiki.sh
+++ run_tiki.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+# Configurar Oracle Instant Client
 export IC_HOME=/opt/oracle/instantclient_23_26
-export LD_LIBRARY_PATH="$IC_HOME:/usr/lib/x86_64-linux-gnu"
-ldd /opt/oracle/instantclient_23_26/libsqora.so.23.1
+export LD_LIBRARY_PATH="$IC_HOME:$LD_LIBRARY_PATH"
+
+# Activar entorno virtual e instalar dependencias
 source .venv/bin/activate
-pip install -r requirements.txt > /dev/null
-#exec streamlit run app.py
+pip install -q -r requirements.txt
+
+# Ejecutar aplicación
 exec python -m src.app
