8092f9167a649bd90332ccbfc2ae57c5d6ec1c1c
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2026-03-10 17:31:26 +0100

    little update of run script

--- run_tiki.sh
+++ run_tiki.sh
@@ -4,9 +4,15 @@
 export IC_HOME=/opt/oracle/instantclient_23_26
 export LD_LIBRARY_PATH="$IC_HOME:$LD_LIBRARY_PATH"
 
+# Check if the virtual environment exists; if not, create it
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
+
 # Activar entorno virtual e instalar dependencias
 source .venv/bin/activate
 pip install -q -r requirements.txt
 
 # Ejecutar aplicación
-exec python -m src.app
+exec python3 -m src.app
